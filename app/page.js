8c22a0c630522d51aba5f7a4f5415ec26b58364d
WebWithRathor <devrajrathor143@gmail.com>
2024-06-14 23:21:34 +0530

    first commit

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex items-center justify-center h-screen text-3xl font-mono font-semibold text-green-200'>Hey!! NEXT.JS</div>
+  )
+}
+
+export default page
