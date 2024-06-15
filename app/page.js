a01c44cb26ac02210c8fce4f412c807b79e8e8d4
WebWithRathor <devrajrathor143@gmail.com>
2024-06-15 22:31:12 +0530

    ...

--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
+"use client"
 import React, { useState } from 'react'
 
 const page = () => {
-  const [Name, setName] = useState('')
+  const [Name, setName] = useState('Nobody')
   return (
     <>
-      <div className='flex items-center justify-center h-screen text-3xl font-mono font-semibold text-green-200'>Hey!! NEXT.JS</div>
-      <input type='text' className='px-6 py-3 bg-transparent border-3 border-zinc-200'/>
+      <div className='flex flex-col gap-3 items-center justify-center h-screen text-3xl font-mono font-semibold text-green-200'>
+       <h1> Hey!! {Name}</h1>
+      <input type='text' onChange={(e)=>setName(e.target.value)} placeholder='Enter Your Name..' className='px-6 py-3  bg-transparent border-2 border-zinc-200'/>
+      </div>
+      
       </>
 
       )
