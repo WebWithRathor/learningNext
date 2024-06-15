39cfee1e8fe74c02f8ff0af81f2f59ce96eb39a6
WebWithRathor <devrajrathor143@gmail.com>
2024-06-15 22:26:41 +0530

    use state added

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const page = () => {
+  const [Name, setName] = useState('')
   return (
-    <div className='flex items-center justify-center h-screen text-3xl font-mono font-semibold text-green-200'>Hey!! NEXT.JS</div>
-  )
+    <>
+      <div className='flex items-center justify-center h-screen text-3xl font-mono font-semibold text-green-200'>Hey!! NEXT.JS</div>
+      <input type='text' className='px-6 py-3 bg-transparent border-3 border-zinc-200'/>
+      </>
+
+      )
 }
 
-export default page
+      export default page
