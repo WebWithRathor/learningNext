77de6aa9f96947bd569a86e826a5f621da1856df
WebWithRathor <devrajrathor143@gmail.com>
2024-06-16 22:20:05 +0530

    Nested Routing added

--- app/components/Component.jsx
+++ app/components/Component.jsx
@@ -1,10 +1,12 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Component = () => {
     return (
         <>
-        <div className='flex items-center justify-center h-screen w-full'>
+        <div className='flex flex-col gap-4 items-center justify-center h-screen w-full'>
             <h1 className='font-semibold text-3xl'> Hey!! ContactPage</h1>
+            <Link href='/contact/help'>Next</Link>
         </div>
         </>
   
---
+++ app/contact/help/page.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+
+const page = () => {
+    return (
+        <div className='flex flex-col gap-4 items-center justify-center h-screen w-full'>
+            <h1 className='font-semibold text-3xl'> Hey!! HelpPage</h1>
+        </div>
+    )
+}
+
+export default page
